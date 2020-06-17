7a272ffcb214444b761e112fbc7ecccdb6eab430
Tan Nguyen <tan.nguyen2@halliburton.com>
2020-06-17 17:11:32 -0500

    combine test and train dataset

--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,29 @@
 library(dplyr)
 library(tidyr)
 
+readData <- function(type = "test") {
+  fileNames <- c("subject_", "y_", "X_")
+  fileNames <- sapply(fileNames, paste, type, ".txt", sep= "")
+  filePaths <- file.path("UCI HAR Dataset", type, fileNames)
+  
+  subjects <- tibble(read.csv(filePaths[1], header = F))
+  names(subjects) <- c("subjectid")
+  
+  labels <- tibble(read.csv(filePaths[2], header = F))
+  names(labels) <- c("id")
+  
+  set <- tibble(read.csv(filePaths[3], header = F, sep = "", numerals = "no.loss"))
+  names(set) <- featureLabels
+  
+  # bug - see why it's only has 480 variables
+  data <- left_join(labels, activitiesLabels)
+  data <- mutate(data, subjects, set) 
+  data$type <- type
+  data
+}
+
 activitiesLabels <- tibble(read.csv("UCI HAR Dataset/activity_labels.txt", sep = " ", header = F))
 names(activitiesLabels) <- c("id", "activity")
 featureLabels <- tibble(read.csv("UCI HAR Dataset/features.txt", header = F, sep = " "))$V2
-                        
 
-testSubjects <- tibble(read.csv("UCI HAR Dataset/test/subject_test.txt", header = F))
-names(testSubjects) <- c("subjectid")
-
-testLabels <- tibble(read.csv("UCI HAR Dataset/test/y_test.txt", header = F))
-testLabels <- tibble(testLabels)
-names(testLabels) <- c("id")
-
-
-testSet <- tibble(read.csv("UCI HAR Dataset/test/X_test.txt", header = F, sep = "", numerals = "no.loss"))
-names(testSet) <- featureLabels
-
-data <- left_join(testLabels, activitiesLabels)
-data <- mutate(data, testSubjects, testSet)
+data <- rbind(readData("test"), readData("train"))
