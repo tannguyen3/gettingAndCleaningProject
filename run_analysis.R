587defa73a4c4705cae493b653d59ea15c1fbf73
Tan Nguyen <tan.nguyen2@halliburton.com>
2020-06-17 14:27:01 -0500

    read test set and its lables

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,10 @@
 library(dplyr)
 library(tidyr)
-# dataFolder <- "UCI HAR Dataset"
-# testPath <- file.path(dataFolder, "test")
-# trainPath <- file.path(dataFolder, "train")
 
 activitiesLabels <- tibble(read.csv("UCI HAR Dataset/activity_labels.txt", sep = " ", header = F))
 names(activitiesLabels) <- c("id", "activity")
+featureLabels <- tibble(read.csv("UCI HAR Dataset/features.txt", header = F, sep = " "))$V2
+                        
 
 testSubjects <- tibble(read.csv("UCI HAR Dataset/test/subject_test.txt", header = F))
 names(testSubjects) <- c("subjectid")
@@ -14,8 +13,9 @@ testLabels <- tibble(read.csv("UCI HAR Dataset/test/y_test.txt", header = F))
 testLabels <- tibble(testLabels)
 names(testLabels) <- c("id")
 
-# data <- rbind(testSubjects, testLabels)
-data <- left_join(testLabels, activitiesLabels)
-data <- mutate(data, subject = testSubjects)
 
-# testSet <- tibble(read.csv("UCI HAR Dataset/test/X_test.txt", header = F, sep = " "))
+testSet <- tibble(read.csv("UCI HAR Dataset/test/X_test.txt", header = F, sep = "", numerals = "no.loss"))
+names(testSet) <- featureLabels
+
+data <- left_join(testLabels, activitiesLabels)
+data <- mutate(data, testSubjects, testSet)
